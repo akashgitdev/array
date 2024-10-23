ac50c03d9686cda246ecc6afe853661fef4c7fc3
akashgitdev <nagaakashduddupudi1712@email.com>
2024-10-23 11:08:47 +0530

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var arr=[1,2,3,4,5];
+console.log(arr.push(6))
+console.log(arr)
+console.log(arr.pop())
+console.log(arr)
+console.log(arr.shift())
+console.log(arr)
+console.log(arr.unshift(1))
+console.log(arr)
+console.log (arr.join(""))
+console.log(arr)
+
+b;
+b;
+b;
+b;
